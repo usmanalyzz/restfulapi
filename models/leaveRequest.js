78873babebce553e629761844f0dad50918988cc
usmanalyzz <95336022+usmanalyzz@users.noreply.github.com>
2024-08-14 03:34:06 +0500

    Leave Request API added

---
+++ models/leaveRequest.js
@@ -0,0 +1,42 @@
+const { type } = require("@hapi/joi/lib/extend");
+const { date } = require("joi");
+const mongoose = require("mongoose");
+
+const leaveSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    leaveType: {
+      type: String,
+      enum: ["Sick", "Casual", "Huj", "Umrah", "Annual", "Other"],
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Leave = mongoose.model("Leave", leaveSchema);
+
+module.exports = { Leave };
